59a87712e3e9f46f936a90c5742c325c090f723d
Nikita <pishkin1112@gmail.com>
2019-05-19 03:22:37 +0300

    #6 commit.ES6. Yoga upd & exersises

---
+++ yogaproject/js/options.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+  class Options {
+    constructor(height, width, bg, fontSize = 14, textAlign = 'justify') {
+      this.height = height;
+      this.width = width;
+      this.bg = bg;
+      this.fontSize = fontSize;
+      this.textAlign = textAlign;
+    }
+    newDiv(text) {
+      let div = document.createElement('div');
+      div.textContent = text;
+      div.style.cssText = `height: ${this.height}px; \
+      width: ${this.width}px; \
+      background: ${this.bg}; \
+      font-size: ${this.fontSize}px; \
+      text-align: ${this.textAlign}; \
+    `;
+      document.body.appendChild(div);
+    }
+  }
+
+  const elem = new Options(300, 400, 'green');
+  elem.newDiv('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus delectus eum repellendus voluptatem labore, cumque pariatur quo inventore autem, enim sapiente facere, ullam quae voluptate aperiam veritatis! Saepe modi aperiam, repellat, sit voluptas officia at dignissimos amet impedit quo nobis, ipsam unde magnam veritatis omnis, pariatur necessitatibus facere odio. Vero.');
+});
+
+
--- yogaproject/js/script.js
+++ yogaproject/js/script.js
@@ -1,7 +1,7 @@
 // window.addEventListener('load') //для нашего окна есть 2 события
 //первое - load: оозначает выполнение скрипта только после полной загрузки 
 //страницы
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded',  () => {
   //это событие для окна говорит о загрузку дерева тегов, т.е. загрузке
   //всей структуры DOM и после этого уже наш скрипт
   'use strict';
@@ -11,42 +11,37 @@ window.addEventListener('DOMContentLoaded', function () {
   //Необходимые составляющие: содержание каждого отдельного таба, кнопки перелючения табов,
   //родитель, который включает всё
   let tab = document.querySelectorAll('.info-header-tab'), //кнопки перекл
-      info = document.querySelector('.info-header'), //родитель кнопок
       tabContent = document.querySelectorAll('.info-tabcontent'); //содержание вкладок
 
 
-  function hideTabContent(a) {
+  // function hideTabContent(a) {
+  //   for (let i = a; i < tabContent.length; i++) {
+  //     tabContent[i].classList.remove('show');
+  //     tabContent[i].classList.add('hide');
+  //   }
+  // }
+  const hideTabContent = (a = 1) => {
     for (let i = a; i < tabContent.length; i++) {
       tabContent[i].classList.remove('show');
       tabContent[i].classList.add('hide');
     }
-  }
-
-  hideTabContent(1);
+  };
+  hideTabContent();
   //вызываем с загрузки ДОМ, чтобы показать одну вкладку, и скрыть все
   //остальные
 
-  function showTabContent(b) {
+  // function showTabContent(b) {
+  //   if (tabContent[b].classList.contains('hide')) {
+  //     tabContent[b].classList.remove('hide');
+  //     tabContent[b].classList.add('show');
+  //   }
+  // }
+  const showTabContent = (b) => {
     if (tabContent[b].classList.contains('hide')) {
       tabContent[b].classList.remove('hide');
       tabContent[b].classList.add('show');
     }
-  }
-
-  info.addEventListener('click', function (event) {
-    let target = event.target;
-    if (target && target.classList.contains('info-header-tab')) {
-      for (let i = 0; i < tab.length; i++) {
-        if (target == tab[i]) { //перебираем все табы до того как найдем
-          //нужный нам таб с индексом(который нам нужен! - да, так мы и
-          //находим этот чертов индекс цели!!!)
-          hideTabContent(0);
-          showTabContent(i);
-          break;
-        }
-      }
-    }
-  });
+  };
 
 
   //пишем таймер(полный разбор)
@@ -58,77 +53,78 @@ window.addEventListener('DOMContentLoaded', function () {
 
   //1
   let deadline = '2019-06-28';
-
   //2
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
-        //парсим дату на милисек
-        //это и будет разници между датами(техн переменная внутри ф-ии)
-        seconds = Math.floor((t / 1000) %  60), //и чтобы получить секунды до одной минуты
-        //берем остаток от деления на 60
-        //метод math который округляет до целого числа
-        minutes = Math.floor((t / 1000 / 60) % 60), //та же хитрость для получ минут
-        hours = Math.floor(( t / ( 1000 * 60 * 60))); //получаем полное кол-во часов до дл
-        // hours = Math.floor((t / 1000 / 60 / 60) % 24), //с учетом до 24 часов в сутках
-        // days = Math.floor((t / 1000 / 60 / 60 / 24)); //дни
-
-        if (hours < 10) {
-          hours = '0' + hours;
-        }
-        if (minutes < 10) {
-          minutes = '0' + minutes;
-        }
-        if (seconds < 10) {
-          seconds = '0' + seconds;
-        }
+      //парсим дату на милисек
+      //это и будет разници между датами(техн переменная внутри ф-ии)
+      seconds = Math.floor((t / 1000) % 60), //и чтобы получить секунды до одной минуты
+      //берем остаток от деления на 60
+      //метод math который округляет до целого числа
+      minutes = Math.floor((t / 1000 / 60) % 60), //та же хитрость для получ минут
+      hours = Math.floor((t / (1000 * 60 * 60))); //получаем полное кол-во часов до дл
+    // hours = Math.floor((t / 1000 / 60 / 60) % 24), //с учетом до 24 часов в сутках
+    // days = Math.floor((t / 1000 / 60 / 60 / 24)); //дни
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
 
     //вынимаем из функции объект со всеми необх данными
     return {
-      'total' : t, //передаем это значение для остановки таймера(в будущем), когда будет (<= 0)
-      'hours' : hours,
-      'minutes' : minutes,
-      'seconds' : seconds
+      'total': t, //передаем это значение для остановки таймера(в будущем), когда будет (<= 0)
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
     };
   }
 
   //3, 4
   function setClock(id, endtime) { //атрибудт id таймера в html и конец таймера
     let timer = document.getElementById(id),
-        hours = timer.querySelector('.hours'),
-        minutes = timer.querySelector('.minutes'),
-        seconds = timer.querySelector('.seconds'),
-        timeInterval = setInterval(updateClock, 1000);
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      // timeInterval = setInterval(updateClock, 1000);
+      timeInterval = setInterval(() => {
+        
+        let t = getTimeRemaining(endtime); //при запуске наш ф-ии updeteClock 
+        //проверяет остаток до дедлайна
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;
 
-    function updateClock() {
-      let t = getTimeRemaining(endtime); //при запуске наш ф-ии updeteClock 
-      //проверяет остаток до дедлайна
-      hours.textContent = t.hours;
-      minutes.textContent = t.minutes;
-      seconds.textContent = t.seconds;
-      
-
-      if (t.total <= 0) {
-        clearInterval(timeInterval);
-        hours.textContent = '00';
-        minutes.textContent = '00';
-        seconds.textContent = '00'; 
-      }
-    } 
+        if (t.total <= 0) {
+          clearInterval(timeInterval);
+          hours.textContent = '00';
+          minutes.textContent = '00';
+          seconds.textContent = '00';
+        }
+      }, 1000);
   }
- 
   setClock('timer', deadline);
 
-  //Модальное окно
 
-  let  overlay = document.querySelector('.overlay'),
-  //избавились от всех лишних переменных, оставили только оверлей
-        isActiveBtn; //об. проверочную переменную
+
+  //Модальное окно
+  let overlay = document.querySelector('.overlay'),
+    //избавились от всех лишних переменных, оставили только оверлей
+    isActiveBtn; //об. проверочную переменную
 
   //делаем стрелочную ф-ю, будет открыв и закрыв модалку
   const bindModal = (overlayStatus, overflowStatus, classListMethod, elem) => {
     //последним аргументом делаем элемент, с которого мы будем удалять класс
-    if (classListMethod == 'add') isActiveBtn = elem;
-    if (!elem) elem = isActiveBtn; //во время вызова закрытия через крестик мы просто
+    if (classListMethod == 'add') {
+      isActiveBtn = elem;
+    } 
+    if (!elem) {
+      elem = isActiveBtn;
+    }  //во время вызова закрытия через крестик мы просто
     //не задаем 4й аргумент ф-ии, и соответственно берем с момента открытия модалки
 
     overlay.style.display = overlayStatus;
@@ -143,26 +139,28 @@ window.addEventListener('DOMContentLoaded', function () {
     //делаем один обрабочик событий на клики во всем боди,
     //при помощи условий будем отлавливать любое событие в любом месте
     let target = e.target;
-    if ( target.classList.contains('more') || target.classList.contains('description-btn')) {
+    if ( target && (target.classList.contains('more') || target.classList.contains('description-btn'))) {
       bindModal('block', 'hidden', 'add', target);
     }
-    if ( target.classList.contains('popup-close')) {
+    if (target && target.classList.contains('popup-close')) {
       bindModal('none', '', 'remove');
+    }
+    if (target && target.classList.contains('info-header-tab')) {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) { //перебираем все табы до того как найдем
+          //нужный нам таб с индексом(который нам нужен! - да, так мы и
+          //находим этот чертов индекс цели!!!)
+          hideTabContent(0);
+          showTabContent(i);
+          break;
+        }
+      }
     }
   });
 
 
 
 
-
-  
-
-
-
-
-
-
-      
 
 
 
